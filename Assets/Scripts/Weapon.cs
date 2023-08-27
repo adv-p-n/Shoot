0e7592758cdca4f7e3ff8a7a51da7653f1bbe2b7
adv-p-n <advaithpnair170302@gmail.com>
2023-08-27 22:40:09 +0530

    Fire VFX added

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 25f;
+    [SerializeField]ParticleSystem muzzleFlashVFX;
     
     void Update()
     {
@@ -19,17 +20,28 @@ public class Weapon : MonoBehaviour
 
     void Shoot()
     {
+        MuzzleFlashFX();
+        ProcessRaycast();
+
+    }
+
+     void MuzzleFlashFX()
+    {
+        muzzleFlashVFX.Play();
+    }
+
+     void ProcessRaycast()
+    {
         RaycastHit hit;
-        if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
             Debug.Log("I hit" + hit.transform.name);
             EnemyHealth enemyHealth = hit.transform.GetComponent<EnemyHealth>();
-            enemyHealth.ReduceHealth(damage);
+            if (enemyHealth) { enemyHealth.ReduceHealth(damage); }
         }
         else
         {
             return;
         }
-        
     }
 }
